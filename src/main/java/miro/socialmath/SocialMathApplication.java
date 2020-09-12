2a05962517271b2d12ca2d32c9d6cfa528822d70
kremir <kremir@o2.pl>
2020-09-12 09:37:11 +0200

    Inital commit

---
+++ src/main/java/miro/socialmath/SocialMathApplication.java
@@ -0,0 +1,13 @@
+package miro.socialmath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMathApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialMathApplication.class, args);
+    }
+
+}
